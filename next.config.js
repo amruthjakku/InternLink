2e2bc5ac5bb2883cac7a995032c5d714e0439d7c
amruth_jakku <amruthjakku@gmail.com>
2025-06-22 10:52:37 +0530

    feat: Add security headers to next.config.js and create robots.txt for improved site security and SEO

--- next.config.js
+++ next.config.js
@@ -14,6 +14,23 @@ const nextConfig = {
           { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
         ],
       },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      }
     ];
   },
 
